7c2aeed63270c8d95e1ee4db2e0aac4331c0f9ef
Shofiqur Rahman <shofi384@gmail.com>
2018-10-27 02:15:23 -0400

    A more refined front front page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="user">User Info goes here</div>
-        <div id="userContent">Users' Decks/Cards go here</div>
-        <div id="otherContent">Other users' Decks/Cards go here</div>
+      # React creates stuffs here
       </div>
     );
   }
